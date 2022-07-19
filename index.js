66c65da3efd568b8e1a1f1f5564a9c481420fc91
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-19 12:40:47 +0200

    solution complete

--- index.js
+++ index.js
@@ -42,32 +42,59 @@ function tellFortune(children, pName, location, job){
     // -   Call that function three times, passing in different values each time.
     // -   **Bonus**: Accept floating point values for amount per day, and round the result to a round number.
     
+function calculateSupply(age,amountPerDay, maxAge){
+    let result = `You will need ${Math.round((maxAge-age||100-age)*amountPerDay*365.25).toLocaleString()} units to last you until you are the ripe old age of ${maxAge||100}. Happy snacking!`;
+    console.log(result);
+    return result;
+    }
+
+    calculateSupply(22, 2);
+    calculateSupply(25, 3);
+    calculateSupply(30, 4);
+
+
+
     
     // ## The Geometrizer
     
-    // Create 2 functions that calculate properties of a circle, using the  [definitions](http://math2.org/math/geometry/circles.htm)  here.
-    
+    // Create 2 functions that calculate properties of a circle, using the  [definitions](http://math2.org/math/geometry/circles.htm)  here.    
     // Create a function called  `calcCircumfrence`:
-    
     // -   Pass the radius to the function.
     // -   Calculate the circumference based on the radius, and output "The circumference is NN".
-    
     // Create a function called  `calcArea`:
-    
     // -   Pass the radius to the function.
     // -   Calculate the area based on the radius, and output "The area is NN".
+
     
+    function calcCircumfrence(r){
+        let p=2*PI*r;
+        console.log('The circumference is',p);
+    }
+
+    function calcArea(r){
+        let a=PI*r*r;
+        console.log('The area is',a);
+    }
     
     // ## The Temperature Converter
-    
-    // It's hot out! Let's make a converter based on  [the steps](http://www.mathsisfun.com/temperature-conversion.html)  here.
-    
-    // Create a function called  `celsiusToFahrenheit`:
-    
+    // It's hot out! Let's make a converter based on  [the steps](http://www.mathsisfun.com/temperature-conversion.html)  here.    
+    // Create a function called  `celsiusToFahrenheit`:    
     // -   Store a celsius temperature into a variable.
-    // -   Convert it to fahrenheit and output "NN°C is NN°F".
-    
-    // Create a function called  `fahrenheitToCelsius`:
-    
+    // -   Convert it to fahrenheit and output "NN°C is NN°F".    
+    // Create a function called  `fahrenheitToCelsius`:    
     // -   Now store a fahrenheit temperature into a variable.
     // -   Convert it to celsius and output "NN°F is NN°C."
+
+    function celsiusToFahrenheit(celcius){
+        let fahrenheit = celcius*9/5 + 32;
+        console.log(celcius + '°C is '+fahrenheit+'°F');
+        return fahrenheit;
+    }
+    celsiusToFahrenheit(0);
+
+    function fahrenheitToCelsius(fahrenheit){
+        let celcius = (fahrenheit - 32)*5/9;
+        console.log(fahrenheit+"°F is "+celcius+"°C.")
+        return celcius;
+    }
+    fahrenheitToCelsius(0);
